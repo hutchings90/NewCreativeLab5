c097cd9fa0a927a3b245ad20abdd07fbf3f5a4d1
Alex.Fowler <jalexfowler@gmail.com>
2016-11-09 22:05:07 +0000

    db updated

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/users');
+require('./models/Users');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var HighScore = mongoose.model('HighScore');
+var User = mongoose.model('User');
+
+
+var highscores
+var db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'connection error:')); 
+db.once('open', function() {
+	console.log('Connected');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.sendFile('SpaceCadet.html', { root: 'public' });
+	res.sendFile('test.html', { root: 'public' });
 	//res.render('SpaceCadet', { title: 'Express' });
 });
+
+router.post('/user', function(req,res,next){
+	var newUser=new User(req.body);
+	newUser.save(function(err, post){
+		if(err) return console.rror(err);
+	
+	});
+	User.find().sort([['score','descending']]).limit(10).exec(function(err, scores){
+  		if (err) return console.error(err);
+		else{
+			console.log(scores);
+		}
+	});
+	User.find(function(err, userList){
+		if (err) return console.error(err);
+   		else {
+    		}
+  	});
+			
+});
+
+router.get('/user', function(req,res,next){
+		User.find().sort([['score','descending']]).limit(10).exec(function(err, scores){
+  		if (err) return console.error(err);
+		else{
+			console.log(scores);
+			res.json(scores);
+		}
+	});
+});
+
+
 
 module.exports = router;
