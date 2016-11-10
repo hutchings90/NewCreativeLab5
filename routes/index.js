3bdffe25afe00756e30f557d237c4d99f170e3a4
hutchings90 <hutchings90@gmail.com>
2016-11-10 01:40:56 +0000

    Remove console.log

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ var highscores
 var db = mongoose.connection; 
 db.on('error', console.error.bind(console, 'connection error:')); 
 db.once('open', function() {
-	console.log('Connected');
 });
 
 /* GET home page. */
